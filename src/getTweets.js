bf3eda6d660ef5f648d4b3a5a22e44d12cc1d5b7
rashi-sahu <rashi.sahu13@gmail.com>
2021-03-03 22:49:53 +0530

    Fetched tweets list from api

--- src/getTweets.js
+++ src/getTweets.js
@@ -1,27 +1,49 @@
 import React from 'react';
+const axios = require("axios");
 
 class GetTweets extends React.Component {
+    constructor(props) {
+        console.log("come");
+        super(props);
+        this.state = {
+            tweets: [],
+            error: null
+        };
+    }
 
-    renderTweets(i) {
-        return (
-            <li>{`Tweets${i}`}</li>
-        );
+    callApi = async () => {
+        console.log("comesdzczfsfs1");
+        try {
+            const response = await axios.get(`http://127.0.0.1:8000/api/rashi`, { crossDomain: true });
+            this.setState({
+                tweets: response.data,
+            });
+            console.log(this.state.tweets)
+        }
+        catch (e) {
+            this.setState({
+                error: e,
+            });
+        }
+    }
+
+    componentDidMount() {
+        this.callApi();
     }
 
     render() {
-        return (
-        <div>
-            {this.renderTweets(0)}
-            {this.renderTweets(1)}
-            {this.renderTweets(2)}
-            {this.renderTweets(3)}
-            {this.renderTweets(4)}
-            {this.renderTweets(5)}
-            {this.renderTweets(6)}
-            {this.renderTweets(7)}
-            {this.renderTweets(8)}
-        </div>
-        );
+        if(!this.state.error){
+            return (
+                <ul>
+                    {this.state.tweets.map(d => (<li>{d.description}</li>))}
+                </ul>
+            );
+        }
+        else{
+            return(
+                <p>{JSON.stringify(this.state.error.message)}</p>
+            );
+        }
     }
 }
 
